5f4cc92a0fd73da13163b506a0657084b910f04d
Filippo Garota <filogarota101@gmail.com>
2021-03-12 16:29:36 +0100

    Aggiunte funzioni

--- main.js
+++ main.js
@@ -1,41 +1,66 @@
 $(function(){
 
-  $('.prev').click(function(){
-    // scorrimento immagini all'indietro
-    var imgActive = $('.images img.active')
-    if (imgActive.hasClass('first') == true) {
-     $('.images img.last').addClass('active');
-    }
-    imgActive.removeClass('active');
-    imgActive.prev().addClass('active');
-    // scorrimento dei bullet all'indietro
-    var bulletActive = $('.nav > i.active');
-    if (bulletActive.hasClass("first") == true) {
-      $(".nav > i.last").addClass('active');
-    }
-    bulletActive.removeClass('active');
-    bulletActive.prev().addClass('active');
+  $('.prev').click(
+    function(){
+      prevImg();
+      prevBullet();
   });
 
 
-  $('.next').click(function(){
-    // scorrimento immagini in avanti
-    var imgActive = $('.images img.active')
-    if (imgActive.hasClass('last') == true) {
-      $('.images img.first').addClass('active');
-    }
-    imgActive.removeClass('active');
+  $('.next').click(
+  function() {
+    nextImg();
+    nextBullet();
+  });
+
+
+
+
+
+
+
+// creo delle funzioni per fare le stesse cose
+
+function nextImg () {
+  var imgActive = $('.images img.active')
+  imgActive.removeClass('active');
+  if (imgActive.hasClass('last') == true) {
+    $('.images img.first').addClass('active');
+  } else {
     imgActive.next().addClass('active');
-    // scorrimento dei bullet in avanti
-    var bulletActive = $('.nav > i.active');
-    if (bulletActive.hasClass("last") == true) {
-      $(".nav > i.first").addClass('active');
-    }
-    bulletActive.removeClass('active');
+  }
+};
+
+function prevImg() {
+  // scorrimento immagini all'indietro
+  var imgActive = $('.images img.active')
+  imgActive.removeClass('active');
+  if (imgActive.hasClass('first') == true) {
+   $('.images img.last').addClass('active');
+ } else {
+   imgActive.prev().addClass('active');
+ }
+}
+
+function nextBullet() {
+  // scorrimento dei bullet in avanti
+  var bulletActive = $('.nav > i.active');
+  bulletActive.removeClass('active');
+  if (bulletActive.hasClass("last") == true) {
+    $(".nav > i.first").addClass('active');
+  } else {
     bulletActive.next().addClass('active');
+  }
+}
 
-
-  });
-
-
+function prevBullet() {
+  // scorrimento dei bullet all'indietro
+  var bulletActive = $('.nav > i.active');
+  bulletActive.removeClass('active');
+  if (bulletActive.hasClass("first") == true) {
+    $(".nav > i.last").addClass('active');
+  }else {
+    bulletActive.prev().addClass('active');
+  }
+}
 });
