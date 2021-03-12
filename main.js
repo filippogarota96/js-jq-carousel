4b5e4280c2739ea57d597fb98280d98c9cd6b72c
Filippo Garota <filogarota101@gmail.com>
2021-03-12 15:24:38 +0100

    Scorrimento delle immagini

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
 $(function(){
 
   $('.prev').click(function(){
+    var imgActive = $('.images img.active')
+    if (imgActive.hasClass('first') == true) {
+     $('.images img.last').addClass('active');
+    }
+    imgActive.removeClass('active');
+    imgActive.prev().addClass('active');
 
   });
 
 
   $('.next').click(function(){
+    var imgActive = $('.images img.active')
+    if (imgActive.hasClass('last') == true) {
+      $('.images img.first').addClass('active');
+    }
+    imgActive.removeClass('active');
+    imgActive.next().addClass('active');
 
-
-    
   });
-
-
-
-
 
 });
